07cecc52984c4aaef5b44d2775687a664967d139
AloneVera <ayberklove@gmail.com>
2024-03-23 17:48:58 +0300

    Co-authored-by: Ayberk <rootroxox@users.noreply.github.com>

---
+++ Courses/Course2/src/App.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Let user enter a number and then print out wether given number is even or
+        // odd.
+
+        // Scanner scan = new Scanner(System.in);
+
+        // System.out.print("Enter a number: ");
+        // int number = scan.nextInt();
+        // scan.nextLine();
+
+        // if (number % 2 == 1) {
+        //     System.out.println("odd");
+        // } else {
+        //     System.out.println("even");
+        // }
+
+        // Let user enter 3 numbers and then print out the greatest one.
+
+        // int biggestNumber;
+
+        // Scanner scan = new Scanner(System.in);
+
+        // System.out.print("Enter a number:");
+        // biggestNumber = scan.nextInt();
+        // scan.nextLine();
+
+        // System.out.print("Enter a number:");
+        // int temp = scan.nextInt();
+        // biggestNumber = (biggestNumber < temp) ? temp : biggestNumber;
+
+        // System.out.print("Enter a number:");
+        // temp = scan.nextInt();
+        // biggestNumber = (biggestNumber < temp) ? temp : biggestNumber;
+
+        // System.out.println("Biggest ==> " + biggestNumber);
+    }
+}
