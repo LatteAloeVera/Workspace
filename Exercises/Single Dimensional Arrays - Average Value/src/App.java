2a75e1a309f3d2a413046a88ed2bfea36dae6000
AloneVera <ayberklove@gmail.com>
2024-03-30 15:53:03 +0300

    Average Value exercises

--- Exercises/Single Dimensional Arrays - Average Value/src/App.java
+++ Exercises/Single Dimensional Arrays - Average Value/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers will you enter?:");
+        
+        int k = scan.nextInt();
+        float sum = 0;
+        scan.nextLine();
+
+        int[] nums = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            System.out.print("Enter a number: ");
+            nums[i] = scan.nextInt();
+            scan.nextLine();
+
+            sum += nums[i];
+        }
+
+        System.out.println("Average value in this array is \"" + sum / k + "\" !");
     }
 }
