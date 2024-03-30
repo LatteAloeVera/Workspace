3f45d06cf4bdc6c36a4beb29bf5868305e79fc83
AloneVera <ayberklove@gmail.com>
2024-03-30 19:28:20 +0300

    prime numbers exercises

--- Exercises/For Loop - Prime Number Check/src/App.java
+++ Exercises/For Loop - Prime Number Check/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int number = scan.nextInt();
+        int n = 0;
+        scan.nextLine();
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                n++;
+            }
+        }
+
+        if (n == 2) {
+            System.out.println("This is a prime number!");
+        }
+        else {
+            System.out.println("This is NOT a prime number!");
+        }
+
+        // How to optimise this program?
+        // Multidimensional Arrays
     }
 }
