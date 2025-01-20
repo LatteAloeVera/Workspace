1da8a346df541b89d5de560ec7e00ec51a67ff80
AloneVera <ayberklove@gmail.com>
2025-01-20 22:49:39 +0300

    updated models

---
+++ FullStack/Exercises/Backend/library-management-system/src/main/java/com/example/library_management_system/Model/Author.java
@@ -0,0 +1,50 @@
+package com.example.library_management_system.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "authors")  
+public class Author {
+    @Id
+    @OneToMany(mappedBy = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String nationality;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ FullStack/Exercises/Backend/library-management-system/src/main/java/com/example/library_management_system/Model/Book.java
@@ -0,0 +1,83 @@
+package com.example.library_management_system.Model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is mandatory")
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "Category is mandatory")
+    @Column(nullable = false)
+    private String category;
+
+    @PastOrPresent // making sure the given time is valid
+    @Column
+    private Date publicationDate;
+
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Long authorId;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String category, Date publicationDate, Long authorId) {
+        this.title = title;
+        this.category = category;
+        this.publicationDate = publicationDate;
+        this.authorId = authorId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+}
