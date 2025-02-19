9dc3683902797175ef90704ac59f20f4097f2882
AloneVera <ayberklove@gmail.com>
2025-02-19 18:05:15 +0300

    finished services

--- FullStack/Exercises/Backend/Auth/library/src/main/java/com/example/library/service/BorrowService.java
+++ FullStack/Exercises/Backend/Auth/library/src/main/java/com/example/library/service/BorrowService.java
@@ -24,11 +24,57 @@ public class BorrowService {
     UserRepository userRepository;
 
     public String borrowBook(Long userId, Long bookId) {
+        // Checking if user exists
         Optional<User> user = userRepository.findById(userId);
         if (user.isEmpty()) {
-            return "User not found";
+            return "User not found.";
         }
         User u = user.get();
 
+        // Checking if book exists
+        Optional<Book> book = bookRepository.findById(bookId);
+        if (book.isEmpty()) {
+            return "Book not found.";
+        }
+        Book b = book.get();
+
+        // Checking if book is already borrowed or not
+        if (borrowRepository.findByBookIdAndReturnedAtIsNull(bookId).isPresent()) {
+            return "Book named \"" + b.getTitle() + "\" is already borrowed";
+        }
+
+        BorrowRecord newRecord = new BorrowRecord();
+        newRecord.setBook(b);
+        newRecord.setUser(u);
+        newRecord.setBorrowedAt(LocalDateTime.now());
+
+        borrowRepository.save(newRecord);
+        return "Book borrowed successfully!";
+    }
+
+    public String returnBook(Long userId, Long bookId) {
+        // Checking if user exists
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {
+            return "User not found.";
+        }
+        User u = user.get();
+
+        // Checking if book exists
+        Optional<Book> book = bookRepository.findById(bookId);
+        if (book.isEmpty()) {
+            return "Book not found.";
+        }
+        Book b = book.get();
+
+        Optional<BorrowRecord> borrowRecord = borrowRepository.findByUserAndBookAndReturnedAtIsNull(u, b);
+        if (borrowRecord.isEmpty()) {
+            return "This book is not borrowed by this user";
+        }
+        BorrowRecord updatedRecord = borrowRecord.get();
+
+        updatedRecord.setReturnedAt(LocalDateTime.now());
+        borrowRepository.save(updatedRecord);
+        return "Book returned successfully!";
     }
 }
